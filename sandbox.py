ee722931213fa90d4f5a9053b37d9fa2e2c63579
garetethan <15016851+garetethan@users.noreply.github.com>
2020-12-30 22:35:58 -0800

    Remove sandbox

--- sandbox.py
+++
@@ -1,9 +0,0 @@
-import math
-from re import sub
-from decimal import Decimal
-
-expression = input('==> ')
-expression = sub(r'(-?[\d\.]+)', r'Decimal(\1)', expression)
-expression = sub(r'\|(.*?)\|', r'abs(\1)', expression)
-print(f'[DEBUG] After substitution:\n{expression}')
-print(f'[DEBUG] After eval:\n{eval(expression)}')
